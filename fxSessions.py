b459b05369e3022355d0e7bb32e54409b574709e
Wisdom <ezeokoliwisdom@gmail.com>
2023-04-29 20:46:16 +0100

    updated sessions

--- fxSessions.py
+++ fxSessions.py
@@ -6,6 +6,13 @@ nyTimeZone = pytz.timezone("America/New_york")
 y = (x.now().hour - x.now(nyTimeZone).hour)
 z = x.now()
 currentNyhourTime = z.hour - y
+dow = x.now().strftime("%A")
+
+
+def checkAsian():
+    for i in range(19, 21):
+        if currentNyhourTime == i:
+            return "Currently in Asian Trading session"
 
 
 def checkLondon():
@@ -15,21 +22,39 @@ def checkLondon():
 
 
 def checkNewyork():
-    for i in range(7, 9):
+    for i in range(7, 10):
         if currentNyhourTime == i:
             return "Currently in New York Trading session"
 
 
-def session():
+def checkLondonClose():
+    for i in range(10, 12):
+        if currentNyhourTime == i:
+            return "Currently in London Close Trading session"
+
+
+def getSession():
     ln = checkLondon()
     ny = checkNewyork()
+    an = checkAsian()
+    lc = checkLondonClose()
 
-    if ny and ln is not None:
-        print("Currently not in ICT trading session")
+    if an is not None:
+        print(an)
     elif ln is not None:
         print(ln)
     elif ny is not None:
         print(ny)
+    elif lc is not None:
+        print(lc)
+    else:
+        print("Market Closed!! No Trading During the Weekend")
 
 
-session()
+weekend = ["Saturday", "Sunday"]
+for i in weekend:
+    if dow == i:
+        print("Market Closed!! No Trading During the Weekend")
+        break
+    else:
+        getSession()
